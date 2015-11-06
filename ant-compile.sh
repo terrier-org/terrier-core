31cc0ffe4ed932ae8cb4fd27d94839a32b38ef8f
Craig Macdonald <craig.macdonald_AT_glasgow.ac.uk>
2015-11-06 16:57:16 +0000

    fixed antlr build. more sane pom.xml

--- ant-compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm src/core/org/terrier/querying/parser/TerrierQueryParser.java
-rm src/core/org/terrier/querying/parser/TerrierQueryParserTokenTypes.java
-rm src/core/org/terrier/querying/parser/TerrierLexer.java 
-rm src/core/org/terrier/querying/parser/TerrierFloatLexer.java
-
-ant
