8cc75151338ebbf14a869f7baa6e691d76f8188f
Craig Macdonald <craig.macdonald_AT_glasgow.ac.uk>
2018-06-07 00:19:51 +0100

    Moved all module source into modules/

--- bin/anyclass.sh
+++ bin/anyclass.sh
@@ -77,7 +77,7 @@ else
 	CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar
 fi
 
-JAR=`ls $TERRIER_HOME/*/target/terrier-*-*-jar-with-dependencies.jar | xargs| tr ' ' :`
+JAR=`ls $TERRIER_HOME/modules/*/target/terrier-*-*-jar-with-dependencies.jar | xargs| tr ' ' :`
 if [ ! -n "$CLASSPATH" ]
 then
 	CLASSPATH=$JAR:$TERRIER_ETC/logback.xml
