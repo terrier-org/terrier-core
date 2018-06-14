480f1684a70901b3690f6f7e55a9665eed10624f
Craig Macdonald <craig.macdonald_AT_glasgow.ac.uk>
2018-06-14 23:46:03 +0100

    testing: make sure fields works in matchopql

--- modules/tests/src/test/java/org/terrier/matching/matchops/TestTRECQueryingMatchOpQL.java
+++ modules/tests/src/test/java/org/terrier/matching/matchops/TestTRECQueryingMatchOpQL.java
@@ -1,5 +1,7 @@
 package org.terrier.matching.matchops;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.terrier.applications.batchquerying.SingleLineTRECQuery;
 import org.terrier.applications.batchquerying.TRECQuerying;
@@ -12,13 +14,17 @@ public class TestTRECQueryingMatchOpQL extends ApplicationSetupBasedTest {
 
 	@Test public void testTopics() throws Exception
 	{
-		Index index = IndexTestUtils.makeIndexBlocks(new String[]{"doc1"}, new String[]{"the fox jumped"});
+		ApplicationSetup.setProperty("FieldTags.process", "TITLE");
+		ApplicationSetup.setProperty("termpipelines", "");
+		Index index = IndexTestUtils.makeIndexFieldsBlocks(new String[]{"doc1"}, new String[]{"the fox jumped <TITLE>over</TITLE>"});
+		assertEquals(1, index.getCollectionStatistics().getNumberOfFields());
 		ApplicationSetup.setProperty("trec.topics.parser", SingleLineTRECQuery.class.getName());
 		ApplicationSetup.setProperty("SingleLineTRECQuery.tokenise", "false");
 		ApplicationSetup.setProperty("trec.topics.matchopql", "true");
 		String f = super.writeTemporaryFile("x.topics", new String[]{
 				"1 fox",
-				"2 #1(fox jumped)"
+				"2 #1(fox jumped)",
+				"3 over.TITLE"
 		});
 		ApplicationSetup.setProperty("trec.topics", f);
 		TRECQuerying tq = new TRECQuerying(index.getIndexRef());
