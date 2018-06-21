0a24e54f5e9ef7b29e0d9138e773ca111ca6b81c
richardm <richardm@dcs.gla.ac.uk>
2018-06-21 15:28:54 -0400

    Added fix for TR-376

--- modules/realtime/src/main/java/org/terrier/realtime/memory/MemoryCompressedMetaIndex.java
+++ modules/realtime/src/main/java/org/terrier/realtime/memory/MemoryCompressedMetaIndex.java
@@ -58,7 +58,7 @@ import org.terrier.utility.ArrayUtils;
  * <p><b>Properties</b></p>
  * <ul><li>indexer.meta.forward.keys</tt> - key names to store in the meta index</li>
  * <li>indexer.meta.forward.keylens</tt> - max key lengths for keys to store (this is ignored unless metaindex.crop is set)</li>
- * <li>metaindex.crop</tt> - should the content to store be cropped down to the length specified in indexer.meta.forward.keylens?</li>
+ * <li>metaindex.compressed.crop.long</tt> - should the content to store be cropped down to the length specified in indexer.meta.forward.keylens?</li>
  * </ul>
  * 
  * @author Richard McCreadie, Dyaa Albakour
