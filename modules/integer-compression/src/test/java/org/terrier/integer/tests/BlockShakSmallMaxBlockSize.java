e20d88a78166c68129fb9894f82ee53bf9d318a8
Craig Macdonald <craig.macdonald_AT_glasgow.ac.uk>
2018-06-21 13:36:50 +0100

    Add copyright headers

--- modules/integer-compression/src/test/java/org/terrier/integer/tests/BlockShakSmallMaxBlockSize.java
+++ modules/integer-compression/src/test/java/org/terrier/integer/tests/BlockShakSmallMaxBlockSize.java
@@ -1,3 +1,28 @@
+/*
+ * Terrier - Terabyte Retriever 
+ * Webpage: http://terrier.org 
+ * Contact: terrier{a.}dcs.gla.ac.uk
+ * University of Glasgow - School of Computing Science
+ * http://www.gla.ac.uk/
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is BlockShakSmallMaxBlockSize.java.
+ *
+ * The Original Code is Copyright (C) 2017-2018 the University of Glasgow.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Craig Macdonald
+ */
 package org.terrier.integer.tests;
 
 import org.terrier.tests.BlockMaxBlocksShakespeareEndToEndTest.MaxBlockChecks;
