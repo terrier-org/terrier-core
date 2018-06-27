98e9541a931192ddbc918908f17d1e0229427185
Craig Macdonald <craig.macdonald_AT_glasgow.ac.uk>
2018-06-27 12:59:56 +0100

    better logging (for purposes of spark)

--- modules/rest-client/src/main/java/org/terrier/restclient/RestClientManagerBuilder.java
+++ modules/rest-client/src/main/java/org/terrier/restclient/RestClientManagerBuilder.java
@@ -39,6 +39,8 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terrier.querying.IndexRef;
 import org.terrier.querying.Manager;
 import org.terrier.querying.ManagerFactory;
@@ -46,14 +48,19 @@ import org.terrier.querying.ScoredDoc;
 import org.terrier.querying.ScoredDocList;
 import org.terrier.querying.SearchRequest;
 
-/** This class facilitates a Manager to be obtained for a remote HTTP 
- *  REST index reference. There is NO NEED to refer to the class directly -
- *  It is sufficient that terrier-rest-client is included in the classpath.
- *  <pre><code>
+/**
+ * This class facilitates a Manager to be obtained for a remote HTTP REST index
+ * reference. There is NO NEED to refer to the class directly - It is sufficient
+ * that terrier-rest-client is included in the classpath.
+ * 
+ * <pre>
+ * <code>
  *  IndexRef ref = IndexRef.of("http://server:8080/");
  *  Manager m = ManagerFactory.from(ref);
- *  </code></pre>
- *  @since 5.0
+ *  </code>
+ * </pre>
+ * 
+ * @since 5.0
  */
 public class RestClientManagerBuilder implements ManagerFactory.Builder {
 
@@ -66,15 +73,13 @@ public class RestClientManagerBuilder implements ManagerFactory.Builder {
 	public Manager fromIndex(IndexRef ref) {
 		return new RESTManagerProxy(ref);
 	}
-	
-	class RESTRequest implements SearchRequest
-	{
+
+	class RESTRequest implements SearchRequest {
 		private static final long serialVersionUID = 1L;
 		String qid, query;
-		Map<String,String> controls = new HashMap<>();
+		Map<String, String> controls = new HashMap<>();
 		ScoredDocList results;
 		long starttime;
-		
 
 		@Override
 		public void addMatchingModel(String MatchingModelName,
@@ -158,17 +163,17 @@ public class RestClientManagerBuilder implements ManagerFactory.Builder {
 		public Map<String, String> getControls() {
 			return controls;
 		}
-		
+
 	}
-	
-	class RESTManagerProxy implements Manager
-	{
+
+	class RESTManagerProxy implements Manager {
+		protected final Logger logger = LoggerFactory.getLogger(RESTManagerProxy.class);
 		IndexRef ref;
-		
-		RESTManagerProxy(IndexRef _ref){
+
+		RESTManagerProxy(IndexRef _ref) {
 			this.ref = _ref;
 		}
-		
+
 		@Override
 		public SearchRequest newSearchRequest() {
 			return new RESTRequest();
@@ -198,69 +203,82 @@ public class RestClientManagerBuilder implements ManagerFactory.Builder {
 
 		@Override
 		public void setProperty(String key, String value) {
-			throw new UnsupportedOperationException("sorry, the rest client and server do no yet support changing properties");
+			throw new UnsupportedOperationException(
+					"sorry, the rest client and server do no yet support changing properties");
 		}
 
 		@Override
 		public void setProperties(Properties p) {
-			throw new UnsupportedOperationException("sorry, the rest client and server do no yet support changing properties");
+			throw new UnsupportedOperationException(
+					"sorry, the rest client and server do no yet support changing properties");
 		}
 
 		@Override
 		public void runSearchRequest(SearchRequest srq) {
+			logger.info("Requesting results for query " + srq.getQueryID());
 			String url = null;
-			try{
+			try {
 				srq.setStartedProcessingTime(System.currentTimeMillis());
-				url = ref.toString()+ "/search/trec?" + "query=" + URLEncoder.encode(srq.getOriginalQuery(), "UTF-8");
-				final RESTRequest rrq = (RESTRequest)srq;
-				Map<String,String> controls = rrq.controls;
-				//controls
-				if (controls.size() > 0)
-				{
-					url = url + "&controls=" + URLEncoder.encode(
-						controls.entrySet().stream().map(entry -> entry.getKey() + ":" + entry.getValue()).collect(Collectors.joining(";")), "UTF-8");
+				url = ref.toString() + "/search/trec?" + "query="
+						+ URLEncoder.encode(srq.getOriginalQuery(), "UTF-8");
+				final RESTRequest rrq = (RESTRequest) srq;
+				Map<String, String> controls = rrq.controls;
+				// controls
+				if (controls.size() > 0) {
+					url = url
+							+ "&controls="
+							+ URLEncoder.encode(
+									controls.entrySet()
+											.stream()
+											.map(entry -> entry.getKey() + ":"
+													+ entry.getValue())
+											.collect(Collectors.joining(";")),
+									"UTF-8");
 				}
-//				//wmodel
-//				if (rrq.hasControl(CONTROL_WMODEL))
-//				{
-//					url += "&wmodel="+rrq.getControl(CONTROL_WMODEL);
-//				}
-//				//matching
-//				if (rrq.hasControl(CONTROL_MATCHING))
-//				{
-//					url += "&matching="+rrq.getControl(CONTROL_MATCHING);
-//				}
-				//qid
-				if (rrq.qid != null)
-				{
-					url += "&qid="+rrq.qid;
+				// //wmodel
+				// if (rrq.hasControl(CONTROL_WMODEL))
+				// {
+				// url += "&wmodel="+rrq.getControl(CONTROL_WMODEL);
+				// }
+				// //matching
+				// if (rrq.hasControl(CONTROL_MATCHING))
+				// {
+				// url += "&matching="+rrq.getControl(CONTROL_MATCHING);
+				// }
+				// qid
+				if (rrq.qid != null) {
+					url += "&qid=" + rrq.qid;
 				}
-				
+
+				logger.debug("URL is " + url);
 				HttpClient httpclient = new DefaultHttpClient();
 				HttpGet httpGet = new HttpGet(url);
 				HttpResponse response = httpclient.execute(httpGet);
 				BufferedReader br = null;
-				try{
+				try {
 					int code = response.getStatusLine().getStatusCode();
 					if (code != 200)
-						throw new IOException("Could not contact REST server at "+url+" : " + code);
-					
-					br = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-					
-									
+						throw new IOException(
+								"Could not contact REST server at " + url
+										+ " : " + code);
+
+					br = new BufferedReader(new InputStreamReader(response
+							.getEntity().getContent(), "UTF-8"));
+
 					ScoredDocList rtr = new ScoredDocListType();
-					Map<String,Integer> metaOffset = new HashMap<String,Integer>();
+					Map<String, Integer> metaOffset = new HashMap<String, Integer>();
 					metaOffset.put("docno", 0);
-					
-					String line;	
-					while ( (line = br.readLine()) != null)
-					{
+
+					String line;
+					while ((line = br.readLine()) != null) {
 						line = line.trim();
 						String[] parts = line.split("\\s+", 6);
-						rtr.add(new ScoredDoc(0, Double.parseDouble(parts[4]), (short)0, new String[]{parts[2]}, metaOffset));
+						rtr.add(new ScoredDoc(0, Double.parseDouble(parts[4]),
+								(short) 0, new String[] { parts[2] },
+								metaOffset));
 					}
-					
-					((RESTRequest)srq).results = rtr;
+
+					((RESTRequest) srq).results = rtr;
 				} finally {
 					if (br != null)
 						br.close();
@@ -268,24 +286,25 @@ public class RestClientManagerBuilder implements ManagerFactory.Builder {
 			} catch (Exception e) {
 				throw new RuntimeException("Could not access " + url, e);
 			}
+			logger.info("Finished query " + srq.getQueryID());
 		}
 
 		@Override
 		public IndexRef getIndexRef() {
 			return ref;
 		}
-		
+
 	}
-	
-	static class ScoredDocListType extends ArrayList<ScoredDoc> implements ScoredDocList 
-	{
+
+	static class ScoredDocListType extends ArrayList<ScoredDoc> implements
+			ScoredDocList {
 		private static final long serialVersionUID = 1L;
 
 		@Override
 		public String[] getMetaKeys() {
-			return new String[]{"docno"};
+			return new String[] { "docno" };
 		}
-		
+
 	}
 
 }
