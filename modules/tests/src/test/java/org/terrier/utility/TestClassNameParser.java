4a95aca9f644bfdb447434df1a73b0accb749cdb
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2020-05-12 15:17:28 +0100

    fix unit test

--- modules/tests/src/test/java/org/terrier/utility/TestClassNameParser.java
+++ modules/tests/src/test/java/org/terrier/utility/TestClassNameParser.java
@@ -5,28 +5,26 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 import org.junit.Test;
-//import org.terrier.querying.prediction.RetrievalPredictor;
+import org.terrier.matching.dsms.DocumentScoreModifier;
 
 public class TestClassNameParser {
 
-	// List<RetrievalPredictor> get(String names) {
-	// 	List<RetrievalPredictor> rtr = null;
-	// 	try{
-	// 		rtr = new ClassNameParser<RetrievalPredictor>(
-	// 				names, RetrievalPredictor.class.getPackage().getName(), RetrievalPredictor.class)
-	// 				.parseAll();
-	// 	}catch (Exception e) {
-	// 		throw new RuntimeException(e);
-	// 	}
-	// 	return rtr;
-	// }
+	List<DocumentScoreModifier> get(String names) {
+		List<DocumentScoreModifier> rtr = null;
+		try{
+			rtr = new ClassNameParser<DocumentScoreModifier>(
+					names, DocumentScoreModifier.class.getPackage().getName(), DocumentScoreModifier.class)
+					.parseAll();
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return rtr;
+	}
 	
-	// @Test
-	// public void test() {
-	// 	assertEquals(1, get("QueryScope").size());
-	// 	assertEquals(1, get("org.terrier.querying.prediction.QueryScope").size());
-	// 	assertEquals(2, get("QueryScope,QueryDifficulty").size());
-	// 	assertEquals(1, get("ClarityScore(20)").size());		
-	// }
+	@Test
+	public void test() {
+		assertEquals(1, get("DFRDependenceScoreModifier").size());
+		assertEquals(2, get("MRFDependenceScoreModifier,DFRDependenceScoreModifier").size());		
+	}
 
 }
