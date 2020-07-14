a848bb60d6ce50cdb32192e6e857391e74aa71f9
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2020-07-14 15:13:24 +0100

    fix javadoc build errors

---
+++ modules/index-api/src/main/java/org/terrier/structures/package-info.java
@@ -0,0 +1,7 @@
+/** Provides the interfaces for the index data structures used 
+for retrieval with the Terrier platform.
+<p>
+These include the APIs of the inverted index, the 
+direct index, the lexicon and the document index, as well as the Posting objects</p>
+**/
+package org.terrier.structures;
