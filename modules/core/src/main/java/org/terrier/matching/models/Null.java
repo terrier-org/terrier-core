4db4d423c03500f054299c077535eab27ec419e0
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-01-09 12:00:40 +0000

    added javadoc

--- modules/core/src/main/java/org/terrier/matching/models/Null.java
+++ modules/core/src/main/java/org/terrier/matching/models/Null.java
@@ -1,5 +1,6 @@
 package org.terrier.matching.models;
 
+/** A weighting model that returns 0 for each match. */
 public class Null extends WeightingModel {
 	private static final long serialVersionUID = 1L;
 
