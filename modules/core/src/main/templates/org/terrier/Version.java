adaba48f1e79d3b34e507299d7d321bd7ca6a9e1
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-01-12 18:07:08 +0000

    include build date and user in Terrier's version information

--- modules/core/src/main/templates/org/terrier/Version.java
+++ modules/core/src/main/templates/org/terrier/Version.java
@@ -3,11 +3,12 @@ package org.terrier;
  * Maven automatically populates this with the version number, etc.
  * See http://stackoverflow.com/questions/2469922/generate-a-version-java-file-in-maven 
  * @since 4.1.
- * TODO: when we mavenize the layout, this class should be moved to src/main/templates
  */
 public class Version {
 
 	public static final String VERSION = "${project.version}";
+	public static final String BUILD_DATE = "${timestamp}";
+	public static final String BUILD_USER = "${user.name}";
 
 	public static int getMajorVersion()
 	{
