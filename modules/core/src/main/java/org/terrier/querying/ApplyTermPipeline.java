8ee5fb270d776526de91a182f283aa0150cba2be
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-06-28 12:00:14 +0100

    WIP - stemming config from control and index.

--- modules/core/src/main/java/org/terrier/querying/ApplyTermPipeline.java
+++ modules/core/src/main/java/org/terrier/querying/ApplyTermPipeline.java
@@ -28,6 +28,8 @@ package org.terrier.querying;
 import java.util.Iterator;
 
 import gnu.trove.TIntArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.terrier.matching.MatchingQueryTerms;
 import org.terrier.matching.MatchingQueryTerms.MatchingTerm;
@@ -35,33 +37,56 @@ import org.terrier.matching.matchops.MultiTermOp;
 import org.terrier.matching.matchops.Operator;
 import org.terrier.matching.matchops.SingleTermOp;
 import org.terrier.terms.BaseTermPipelineAccessor;
+import org.terrier.structures.Index;
+import org.terrier.structures.PropertiesIndex;
 import org.terrier.terms.TermPipelineAccessor;
 import org.terrier.utility.ApplicationSetup;
 
 @ProcessPhaseRequisites(ManagerRequisite.MQT)
+/** Applies the TermPipeline to the MatchingQueryTerms object.
+ * The configuration of the TermPipeline is obtained based on the following configurations:
+ * 
+ * 1. A control setting for the query, if present
+ * 2. The "termpiplines" property of the index, if present
+ * 3. Failing that, the "termpipelines" property in ApplicationSetup
+ * 
+ * These configuration rules were introduced in Terrier 5.6. This means that previously where a property was sufficient
+ * to set the stemming configuration at retrieval, now a control is required. However, as the index termpipeline configuration
+ * is used by default, the scenarios where explicit configuration are less frequent. In particular, if you create an index
+ * using a particular stemmer, that stemmer should be automatically picked up from the index (some exceptions - realtime and
+ * composed indices).
+ */
 public class ApplyTermPipeline implements Process {
 
-	TermPipelineAccessor tpa = null;
+	/** Logging error messages */
+	private static Logger LOG = LoggerFactory.getLogger(ApplyTermPipeline.class);
+
+	TermPipelineAccessor defaultTpa = null;
 	String info = null;
+	String defaultPipelineSource = null;
+	String lastPipeline = null;
+	TermPipelineAccessor lastTpa = null;
+	boolean warnNoIndexNoControl = false;
 	
 	public ApplyTermPipeline()
 	{
-		this.load_pipeline(ApplicationSetup.getProperty("termpipelines", "Stopwords,PorterStemmer").trim());
+		this(ApplicationSetup.getProperty("termpipelines", "Stopwords,PorterStemmer").trim());
 	}
 
 	public ApplyTermPipeline(String pipeline)
 	{
-		this.load_pipeline(pipeline);
+		synchronized (this) {
+			defaultTpa = this.load_pipeline(pipeline);
+			defaultPipelineSource = pipeline;
+		}
 	}
 	
 	/** load in the term pipeline */
-	protected void load_pipeline(final String tp)
+	protected TermPipelineAccessor load_pipeline(final String tp)
 	{
 		final String[] pipes = tp.split("\\s*,\\s*");
 		info = "termpipelines=" + tp;
-		synchronized (this) {
-			tpa = new BaseTermPipelineAccessor(pipes);
-		}		
+		return new BaseTermPipelineAccessor(pipes);	
 	}
 	
 	interface Visitor {
@@ -70,11 +95,66 @@ public class ApplyTermPipeline implements Process {
 		boolean visit(MultiTermOp mqt);
 	}
 	
-	
+	TermPipelineAccessor getPipeline(Manager manager, Request q) {
+		
+		assert defaultPipelineSource != null;
+
+		String tp = null;
+		Index index = q.getIndex();
+		boolean hasControl = q.hasControl("termpipelines");
+		boolean noIndexNoControl = false;
+		
+		if (! hasControl && index != null) {
+			if (index instanceof PropertiesIndex) {
+				PropertiesIndex pi = (PropertiesIndex) index;
+				String indexProp = pi.getIndexProperty("termpipelines", null); 
+				if (indexProp != null) {
+					tp = indexProp;
+				} else {
+					// no property found
+					noIndexNoControl = true;
+				}
+			} else {
+				// not a properties index
+				noIndexNoControl = true;
+			}	
+		}
+		if (hasControl)
+		{
+			tp = q.getControl("termpipelines");
+			noIndexNoControl = false;
+		}
+		if (tp == null || defaultPipelineSource.equals(tp)) {
+			
+			//we want to promote the use of controls over ApplicationSetup. This will warn if ApplicationSetup is used.
+			//it will likely only occur for settings of MultiIndex and RealtimeIndex etc.
+			if (noIndexNoControl && ! warnNoIndexNoControl)
+			{
+				LOG.warn("The index has no termpipelines configuration, and no control configuration is found. "+
+					"Defaulting to global termpipelines configuration of '"+defaultPipelineSource+"'. " +  
+					"Set a termpipelines control to remove this warning.");
+				warnNoIndexNoControl = true;
+			}
+			
+			return defaultTpa;
+		}
+		if (tp.equals(lastPipeline))
+		{
+			return lastTpa;
+		}
+		synchronized(this) {
+			lastPipeline = tp;
+			info = tp;
+			return load_pipeline(tp);
+		}
+		
+	}
 	
 	@Override
 	public void process(Manager manager, Request q) {
 		
+		TermPipelineAccessor tpa = getPipeline(manager, q);
+		assert tpa != null;
 		
 		TIntArrayList toDel = new TIntArrayList();
 		int i=-1;
@@ -97,7 +177,10 @@ public class ApplyTermPipeline implements Process {
 			@Override
 			public boolean visit(SingleTermOp sqt) {
 				String origTerm = sqt.getTerm();
-				String newTerm = tpa.pipelineTerm(origTerm);
+				String newTerm;
+				synchronized(tpa) {
+					newTerm = tpa.pipelineTerm(origTerm);
+				}
 				if (newTerm == null)
 					return false;
 				sqt.setTerm(newTerm);
