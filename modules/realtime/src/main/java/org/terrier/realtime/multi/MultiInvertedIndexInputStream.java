20e5355ef51118cbc51dedac01829179e359c6e2
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2023-10-11 12:41:44 +0100

    remove dead code, fix indentation

--- modules/realtime/src/main/java/org/terrier/realtime/multi/MultiInvertedIndexInputStream.java
+++ modules/realtime/src/main/java/org/terrier/realtime/multi/MultiInvertedIndexInputStream.java
@@ -20,30 +20,6 @@ public class MultiInvertedIndexInputStream implements PostingIndexInputStream {
         this.mindex = mindex;
         this.iterLex = iterLex;
     }
-        
-        // List<Iterator<Map.Entry<String,LexiconEntry>> iterLexs;
-        // List<Iterator<Map.Entry<Map.Entry<String,LexiconEntry>,Integer>> iterLexsOffset = new ArrayList<>();
-        // for (int i=0;i<iterLexs.size();i++) {
-        //     iterLexsOffset.add(IteratorUtils.addOffset(iterLexs.get(i), i));
-        // }
-        
-        // var lexIterOffset = IteratorUtils.merge(  
-		// 		// comparator
-		// 		(Map.Entry<Map.Entry<String,LexiconEntry>,Integer> term1, Map.Entry<Map.Entry<String,LexiconEntry>,Integer> term2) -> term1.getKey().getKey().compareTo(term2.getKey().getKey()), 
-		// 		// merger
-		// 		(Map.Entry<Map.Entry<String,LexiconEntry>,Integer> term1, Map.Entry<Map.Entry<String,LexiconEntry>,Integer> term2) -> new MapEntry(  
-		// 			term1.getKey(), 
-		// 			makeMLE(term1.getKey().getValue(), term2.getLey().getValue(), term1.getValue(), term2.getValue() )),
-		// 		// iterators
-		// 		(Iterator<Map.Entry<Map.Entry<String,LexiconEntry>>,Integer>[]) iters.toArray(new Iterator<?>[iters.size()]));
-    //}
-
-    // MultiLexiconEntry makeMLE(LexiconEntry le_a, LexiconEntry le_b, int pos_a, int pos_b) {
-    //     LexiconEntry children = new LexiconEntry[offset.length];
-    //     children[pos_a] = le_a;
-    //     children[pos_b] = le_b;
-    //     return new MultiLexiconEntry(children, 0);
-    // }
 
     public IterablePosting getNextPostings() throws IOException {
         if (! iterLex.hasNext())
@@ -71,13 +47,13 @@ public class MultiInvertedIndexInputStream implements PostingIndexInputStream {
         }
     }
 
-	/** Returns the number of postings in the current IterablePosting object */
-	public int getNumberOfCurrentPostings() {
+    /** Returns the number of postings in the current IterablePosting object */
+    public int getNumberOfCurrentPostings() {
         return le.getNumberOfEntries();
     }
 	
-	/** Returns the pointer associated with the current postings being accessed */
-	public Pointer getCurrentPointer() {
+    /** Returns the pointer associated with the current postings being accessed */
+    public Pointer getCurrentPointer() {
         return le;
     }
 
