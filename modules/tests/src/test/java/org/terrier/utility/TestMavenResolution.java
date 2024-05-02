f8e0470c1acb04afd70e7ce76620804d62fb6745
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2024-05-02 17:59:57 +0100

    assumeTrue not assumeFalse

--- modules/tests/src/test/java/org/terrier/utility/TestMavenResolution.java
+++ modules/tests/src/test/java/org/terrier/utility/TestMavenResolution.java
@@ -59,9 +59,9 @@ public class TestMavenResolution extends ApplicationSetupBasedTest {
 	@Test public void testImportSingleIndirectWithClassifier() throws Exception
 	{
 		// this test fails on Alpine linux on Docker.
-		assumeFalse(! new File("/etc/alpine-release").exists());
+		assumeTrue(! new File("/etc/alpine-release").exists());
 		// this test fails on MacOS M1 arm
-		assumeFalse(! (System.getProperty("os.name").equals("Mac OS X") && System.getProperty("os.arch").equals("aarch64")));
+		assumeTrue(! (System.getProperty("os.name").equals("Mac OS X") && System.getProperty("os.arch").equals("aarch64")));
 		new MavenResolver().initialise("org.nd4j:nd4j-native-platform:1.0.0-M2,org.nd4j:nd4j-native:1.0.0-M2");
 		Class<?> clz = Thread.currentThread().getContextClassLoader().loadClass("org.nd4j.linalg.factory.Nd4j");
 		assertNotNull(clz);
