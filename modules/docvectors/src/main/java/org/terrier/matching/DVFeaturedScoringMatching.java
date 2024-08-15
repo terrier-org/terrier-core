0676a3448b8078154f6e8dc3bf808c16d50a1e27
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2024-08-15 17:42:42 +0100

    refer to DV in javadoc

--- modules/docvectors/src/main/java/org/terrier/matching/DVFeaturedScoringMatching.java
+++ modules/docvectors/src/main/java/org/terrier/matching/DVFeaturedScoringMatching.java
@@ -22,7 +22,8 @@ import org.terrier.structures.postings.BlockPosting;
 import org.terrier.structures.postings.IterablePosting;
 import org.terrier.utility.ApplicationSetup;
 
-/** Uses Direct2InvIndex to make a mock InvertedIndex (from a DirectIndex) for rescoring documents for computing additional features.
+/** Implements the DocVectors style of computing additional features. Uses Direct2InvIndex to make a mock InvertedIndex 
+ * (from a DirectIndex) for rescoring documents for computing additional features.
  * Behaves the same way as FatFeaturedScoringMatching. This class can also support retrieval for only some "tagged" query terms.
  */
 public class DVFeaturedScoringMatching extends FeaturedScoringMatching {
